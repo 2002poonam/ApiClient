f3e1a2fc88ce88d82b128aa9de35cc71e530a2da
Poonam Kumari <2002poonam@gmail.com>
2018-02-17 18:00:18 +0100

    share

---
+++ CallApi/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CallApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdArticles = new System.Windows.Forms.DataGridView();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnPost = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grdArticles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdArticles
+            // 
+            this.grdArticles.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.grdArticles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdArticles.Location = new System.Drawing.Point(-2, 123);
+            this.grdArticles.Name = "grdArticles";
+            this.grdArticles.Size = new System.Drawing.Size(884, 342);
+            this.grdArticles.TabIndex = 0;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(-2, 46);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // btnPost
+            // 
+            this.btnPost.Location = new System.Drawing.Point(90, 46);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(75, 23);
+            this.btnPost.TabIndex = 2;
+            this.btnPost.Text = "Post";
+            this.btnPost.UseVisualStyleBackColor = true;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 91);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 468);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnPost);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.grdArticles);
+            this.Name = "Form1";
+            this.Text = "Assignment";
+            ((System.ComponentModel.ISupportInitialize)(this.grdArticles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdArticles;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ CallApi/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+
+
+
+
+namespace CallApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            lblStatus.Visible = false;
+
+
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+           //Call the webapi 
+            Get();
+
+        }
+
+       
+        /// <summary>
+        /// Get all storeItems from webApi
+        /// </summary>
+        private async void Get()
+        {
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync("http://www.nutidweboffice.com/bunkersgolfhk/api/assignment"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var itemJsonString = await response.Content.ReadAsStringAsync();
+                        List<StoreItem> lstStoreItem;
+                       
+                        lstStoreItem = JsonConvert.DeserializeObject<StoreItem[]>(itemJsonString).ToList();
+
+                        //Populate the gridview
+                        Populate(lstStoreItem);
+
+                    }
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// Method to populate gridview with storeitem list
+        /// </summary>
+        /// <param name="lstStoreItem"></param>
+        private void Populate(List<StoreItem> lstStoreItem)
+        {
+            
+            grdArticles.DataSource = null;
+            grdArticles.Rows.Clear();
+            grdArticles.Refresh();
+            grdArticles.Columns.Clear();
+
+            int i = 0;
+           
+            grdArticles.Columns.Add("ID", "ID");
+            grdArticles.Columns.Add("ItemId", "ItemId");
+            grdArticles.Columns.Add("StoreItemDescriptions", "StoreItemDescriptions");
+            grdArticles.Columns.Add("Sellprice", "Sellprice");
+            grdArticles.Columns.Add("StoreDepartment", "StoreDepartment Name");
+            grdArticles.Columns.Add("Barcode", "Barcode");
+            grdArticles.Columns.Add("StoreItemStorageAmounts", "StorageSpot Description");
+            grdArticles.Columns.Add("ItemNumber", "ItemNumber");
+            grdArticles.Columns.Add("CatalogNumber", "CatalogNumber");
+            grdArticles.Columns.Add("Extra1", "Extra1");
+            grdArticles.Columns.Add("Deleted", "Deleted");
+
+            foreach (var storeItem in lstStoreItem)
+            {
+                
+                i++;
+
+                DataGridViewRow row = new DataGridViewRow();
+
+                row.CreateCells(grdArticles);
+                row.Cells[0].Value = storeItem.Id;
+                row.Cells[1].Value = storeItem.ItemId;
+                row.Cells[2].Value = storeItem.StoreItemDescriptions[0].Description;
+                row.Cells[3].Value = storeItem.StoreItemPrices[0].Sellprice; 
+                row.Cells[4].Value = storeItem.StoreDepartment.StoreDepartmentNames[0].Name;
+                row.Cells[5].Value = storeItem.Barcode;
+                row.Cells[6].Value = storeItem.StoreItemStorageAmounts[0].StorageSpot.Description;
+                row.Cells[7].Value = storeItem.ItemNumber;
+                row.Cells[8].Value = storeItem.CatalogNumber;
+                row.Cells[9].Value = storeItem.Extra1;
+                row.Cells[10].Value = storeItem.Deleted;
+               
+
+                grdArticles.Rows.Add(row);
+                
+            }
+        }
+
+        private void btnPost_Click(object sender, EventArgs e)
+        {
+            //Check storeitems count is 3 or not
+            ItemsInStock();
+        }
+
+        
+        /// <summary>
+        /// Method for Checking storeitems count is 3 or not.And based on that show the status.
+        /// </summary>
+        private async void ItemsInStock()
+        {
+
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync("http://www.nutidweboffice.com/bunkersgolfhk/api/assignment"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var itemJsonString = await response.Content.ReadAsStringAsync();
+                        int cntStoreItem;
+
+
+                        cntStoreItem = JsonConvert.DeserializeObject<StoreItem[]>(itemJsonString).ToList().Count;
+                         
+                       if (cntStoreItem!=3)
+                        {
+                            lblStatus.Visible = true;
+                            
+                            var msgResponse = new HttpResponseMessage(System.Net.HttpStatusCode.NotFound);
+                            lblStatus.Text = "Status:-"+ msgResponse.StatusCode + " -StoreItems count is not equal to 3. ";
+                        }
+                       else
+                        {
+                            lblStatus.Visible = true;
+
+                            var msgResponse = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+                            lblStatus.Text = "Status:-" + msgResponse.StatusCode  + " -StoreItems count is equal to 3. ";
+                        }
+
+
+
+                    }
+                }
+            }
+           
+        }
+
+       
+    }
+}
